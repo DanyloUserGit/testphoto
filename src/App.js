f24e505798366dd406f61370df0586639eec3618
DanyloUserGit <71781037+DanyloUserGit@users.noreply.github.com>
2023-02-19 17:22:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,24 @@ import './App.css';
 import { Addbtn, Camerabtn } from './components/buttons';
 import { Video } from './components/Form';
 import { Username } from './components/names';
-import {useEffect, useState, useRef} from 'react';
+import {useEffect, useState, useRef, useCallback} from 'react';
 import Webcam from 'react-webcam';
 import axios from 'axios';
 function App() {
-  // const axios = require('axios');
   const ApiKey = "c28528763c8b4a7c8f0a000f4f602c8e";
-  const [addWork, setaddWork] = useState('');
+  const [addWork, setaddWork] = useState("");
   const [cameraOn, setcameraOn] = useState("none");
   const webRef = useRef(null);
   const videoConstraints = {
     facingMode: { exact: "environment" }
   };
-  let img = null;
-  const TakePhoto = () => {
-    img = webRef.current.getScreenshot();
-    setaddWork(img);
-  }
   const successCallback = (position) => {
     const {latitude, longitude} = position.coords;
     fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${ApiKey}`)
     .then(response=>response.json())
     .then(data=>{
+      const img = webRef.current.getScreenshot();
+      setaddWork(img);
       console.log(data);
       fetch(('https://photobase-a34b5-default-rtdb.europe-west1.firebasedatabase.app/data/-NOdw0sdfmPqwLwsrRmh.json?id=2'),{
         method: 'PATCH',
@@ -31,7 +27,7 @@ function App() {
         'Content-Type':'application/json'
         },
         body: JSON.stringify({userworks:[{
-          img:webRef.current.getScreenshot(),
+          img:img,
           date:data.timestamp.created_http,
           location: data.results[0].formatted
         }]})
@@ -47,13 +43,20 @@ function App() {
     console.log(resp.data.userworks);
     })
   }
+  // const TakePhoto = useCallback(()=>{
+  //   const img = webRef.current.getScreenshot();
+  //   setaddWork(img);
+  // })
+  
   return (
     <div>
       {getUserWorks(2)}
       <Webcam ref={webRef} audio={false} className={"preview"} style={{display: cameraOn}} videoConstraints={videoConstraints} />
-      <Camerabtn style={{display: cameraOn}} onclickHandle={()=>{
-        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+      <Camerabtn style={{display: cameraOn}} onclickHandle={(e)=>{
+        e.preventDefault();
+        // TakePhoto()
         setcameraOn("none");
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
       }}/>
       <Username margin={5} name={"James Bond"}/>
 
